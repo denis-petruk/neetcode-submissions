dbd9d2211206ff49f9bb80340c1ca22c026773e6
Denis Petruk <deniska.petruk@icloud.com>
2026-08-14 17:58:42 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not self.checker(s[left]):
+                left += 1
+            while right > left and not self.checker(s[right]):
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left, right = left + 1, right - 1
+        return True
+
+    def checker(self, char : str) -> bool:
+        return (ord('A') <= ord(char) <= ord('Z') or
+                ord('a') <= ord(char) <= ord('z') or
+                ord('0') <= ord(char) <= ord('9'))
